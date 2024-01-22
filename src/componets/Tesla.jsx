78123cf34726e183ba577206d4e707933a7ccf2d
natidever <natnael7820@gmail.com>
2024-01-23 00:15:50 +0300

    added BUTTON

--- src/componets/Tesla.jsx
+++ src/componets/Tesla.jsx
@@ -1,10 +1,11 @@
 import tesla from "../assets/tesla.png"
+import Button from "./Button"
 
 function Tesla() {
   return (
     <div className='sm:px-10 sm:py-5 sm:flex-row flex flex-col' >
 
-
+<Button></Button>
     
   
     </div>
