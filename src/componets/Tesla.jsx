febeaa895dd6a1235672e72d544970b16c050af5
natidever <natnael7820@gmail.com>
2024-01-23 00:13:33 +0300

    added confiugre

--- src/componets/Tesla.jsx
+++ src/componets/Tesla.jsx
@@ -4,6 +4,11 @@ function Tesla() {
   return (
     <div className='sm:px-10 sm:py-5 sm:flex-row flex flex-col' >
        <img src= {tesla} className="" alt="" />
+       <img src= {tesla} className="" alt="" />
+       <img src= {tesla} className="" alt="" />
+       <img src= {tesla} className="" alt="" />
+       <img src= {tesla} className="" alt="" />
+       <img src= {tesla} className="" alt="" />
     </div>
   )
 }
